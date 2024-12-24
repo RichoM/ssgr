c3cee3e77ce5b1625624b184dd3f1851b697c64c
RichoM <richi.moran@gmail.com>
2024-12-24 16:51:41 -0300

    Updated project description, url, and license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ssgr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Static Site Generator by Richo"
+  :url "https://github.com/RichoM/ssgr"
+  :license {:name "MIT"
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [io.github.nextjournal/markdown "0.6.157"]]
   :main ^:skip-aot ssgr.core
