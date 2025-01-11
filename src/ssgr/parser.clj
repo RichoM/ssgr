795755dbb4076229a48c4ef17ef6038c49df0a17
RichoM <richi.moran@gmail.com>
2025-01-11 12:25:48 -0300

    Starting to implement parsing of clojure code alongside markdown.
    I added a new parser that tries to parse clojure code using the edamame library. It only invokes the clojure parser if I detect an opening "(", I figured I only need s-expressions anyway and this would simplify the code.

--- src/ssgr/parser.clj
+++ src/ssgr/parser.clj
@@ -1,6 +1,49 @@
 (ns ssgr.parser
-  (:require [petitparser.core :as pp]
-            [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [petitparser.core :as pp]
+            [petitparser.input-stream :as in]
+            [petitparser.results :as r]
+            [edamame.core :as e]))
+
+(defn line-indices [string]
+  (loop [[line & rest] (str/split string #"\n")
+         start 0
+         indices (transient [])]
+    (if line
+      (let [count (count line)
+            stop (+ start count)]
+        (recur
+         rest
+         (inc stop)
+         (conj! indices [start stop])))
+      (persistent! indices))))
+
+(defn advance-stream-to-match! [stream reader source]
+  (let [line-number (e/get-line-number reader)
+        column-number (e/get-column-number reader)
+        position (+ (first (nth (line-indices source)
+                                (dec line-number)))
+                    (dec column-number)
+                    (in/position stream))]
+    (in/reset-position! stream position)))
+
+(defrecord ClojureParser []
+  petitparser.parsers.Parser
+  (parse-on [_ stream]
+    (if (= \( (in/peek stream))
+      (try
+        (let [src (subs (in/source stream) (in/position stream))
+              reader (e/source-reader src)
+              result (e/parse-next reader #_(e/normalize-opts {:all false}))]
+          (advance-stream-to-match! stream reader src)
+          (r/success result))
+        (catch Exception _
+          (r/failure (in/position stream)
+                     (str "Clojure code expected"))))
+      (r/failure (in/position stream)
+                 (str "Literal '(' expected")))))
+
+(def clojure-parser (ClojureParser.))
 
 (set! *unchecked-math* :warn-on-boxed)
 (set! *warn-on-reflection* true)
@@ -14,6 +57,9 @@
   {:type ::text
    :text text})
 
+(defn code [form]
+  {:type ::code
+   :form form})
 
 (defn paragraph [lines]
   {:type ::paragraph
@@ -32,8 +78,10 @@
                    (pp/plus :ws) 
                    (pp/star :inline)
                    (pp/star :ws)]
-     :inline (pp/or :text)
-     :text (pp/flatten (pp/plus :char))
+     :inline (pp/or :text :code)
+     :text (pp/flatten (pp/or (pp/plus-lazy :char :code)
+                              (pp/plus :char)))
+     :code clojure-parser
      :ws (pp/flatten (pp/plus (pp/or \tab \space)))
      :char (pp/predicate #(and (not= % \return)
                                (not= % \newline))
@@ -56,6 +104,7 @@
      :atx-heading (fn [[_ atx _ content]]
                     (heading (count atx) content))
      :text (comp text str/trim)
+     :code code
      :newline (constantly \newline)})
 
   (def parser (pp/compose grammar transformations)))
@@ -67,7 +116,7 @@
   (pp/parse (pp/star (:inline (:parsers parser))) "Richo (+ 3 4)")
   (parse "# Richo (+ 3 4)")
   (parse "# Richo capo")
-  (parse "# Richo\n# (+ 3 4)")
+  (parse "# Richo\n(+ 3 4)")
 
   (partition-by :type [{:type :ssgr.parser/empty-line}
                        {:content [{:text "Richo", :type :ssgr.parser/text}], :level 1, :type :ssgr.parser/heading}
