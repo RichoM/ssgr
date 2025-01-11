d40ef2e3f5ff5d8a9e857366d4cfe9ab4aebdaf8
RichoM <richi.moran@gmail.com>
2025-01-11 12:23:01 -0300

    Improved the parser. Now it can parse paragraphs

--- src/ssgr/parser.clj
+++ src/ssgr/parser.clj
@@ -5,59 +5,104 @@
 (set! *unchecked-math* :warn-on-boxed)
 (set! *warn-on-reflection* true)
 
-(defn heading [level text]
+(defn heading [level content]
   {:type ::heading
    :level level
+   :content content})
+
+(defn text [text]
+  {:type ::text
    :text text})
 
-(defn text-line [text]
-  {:type ::text-line
-   :text text})
 
-(defn empty-line [_]
-  {:type ::empty-line})
+(defn paragraph [lines]
+  {:type ::paragraph
+   :lines lines})
+
+(def empty-line {:type ::empty-line})
 
 (do
   (def grammar
-    {:start (pp/star :line)
-     :line (pp/or :empty-line :atx-heading :text-line)
-     :atx-heading [:ws? (pp/plus \#) :text (pp/optional :newline)]
-     :text (pp/flatten
-            (pp/plus (pp/predicate #(and (not= % \return)
-                                         (not= % \newline))
-                                   "Any char except newline")))
-     :text-line [:text (pp/optional :newline)]
-     ;:text-line (pp/flatten
-     ;            (pp/or [(pp/plus-lazy pp/any :newline) :newline]
-     ;                   (pp/end (pp/plus pp/any))))
-     :empty-line [:ws? :newline]
-     :ws? (pp/star (pp/or "\t" " "))
-     :newline (pp/or "\n" "\n\r")})
+    {:start :lines
+     :lines (pp/separated-by (pp/optional :line) 
+                             :newline)
+     :line (pp/or :atx-heading :inline)
+     :atx-heading [(pp/star :ws) 
+                   (pp/plus \#) 
+                   (pp/plus :ws) 
+                   (pp/star :inline)
+                   (pp/star :ws)]
+     :inline (pp/or :text)
+     :text (pp/flatten (pp/plus :char))
+     :ws (pp/flatten (pp/plus (pp/or \tab \space)))
+     :char (pp/predicate #(and (not= % \return)
+                               (not= % \newline))
+                         "Any char except newline")
+     :newline (pp/or "\r\n" \return \newline )})
 
   (def transformations
-    {:atx-heading (fn [[_ atx text]]
-                    (heading (count atx) text))
-     :text str/trim
-     :text-line (fn [[text _]] (text-line text))
-     :empty-line empty-line})
+    {:lines (fn [lines]
+              (->> lines
+                   (take-nth 2)
+                   (mapv #(if % % empty-line))
+                   (partition-by :type)
+                   (mapcat (fn [group]
+                           (when-let [{:keys [type]} (first group)]
+                             (case type
+                               ::heading group
+                               ::text [(paragraph (vec group))]
+                               []))))
+                   (vec)))
+     :atx-heading (fn [[_ atx _ content]]
+                    (heading (count atx) content))
+     :text (comp text str/trim)
+     :newline (constantly \newline)})
 
   (def parser (pp/compose grammar transformations)))
 
 (defn parse [src] (pp/parse parser src))
 
 (comment
+  
+  (pp/parse (pp/star (:inline (:parsers parser))) "Richo (+ 3 4)")
+  (parse "# Richo (+ 3 4)")
+  (parse "# Richo capo")
+  (parse "# Richo\n# (+ 3 4)")
+
+  (partition-by :type [{:type :ssgr.parser/empty-line}
+                       {:content [{:text "Richo", :type :ssgr.parser/text}], :level 1, :type :ssgr.parser/heading}
+                       {:text "Text", :type :ssgr.parser/text}
+                       {:text "Text 2", :type :ssgr.parser/text}
+                       {:type :ssgr.parser/empty-line}
+                       {:type :ssgr.parser/empty-line}
+                       {:text "Text3", :type :ssgr.parser/text}])
+  (tap> (->> (parse "\n# Richo  \nText\nText 2\n\n\nText3")
+             (partition-all 2)
+             (map (fn [[a b]]
+                    (if (nil? a)
+                      empty-line
+                      a)))))
+  
+  (pp/parse (pp/seq (-> parser :parsers :atx-heading)
+                    (pp/optional pp/any))
+            "\n# Richo  \n")
+
   (require '[portal.api :as p])
 
-  (def p (p/open))
-  (add-tap #'p/submit) ; Add portal as a tap> target
-  \n
+  (def p (p/open {:launcher :vs-code}))
+  (add-tap #'p/submit)
 
+  (tap> (pp/parse (-> parser :parsers :paragraph)
+                  ""))
   (tap> {:richo "CAPO"})
   (tap> (parse "# Richo capo\nTest.\nSegunda línea\n\nSegundo párrafo.\nPrueba\nFin."))
 
-  (doseq  [line (pp/parse parser (slurp "README.md"))]
+  (doseq  [line (parse (slurp "README.md"))]
     (println line))
 
+  (p/close p)
 
 
+  (pp/parse (pp/as-parser [(pp/separated-by pp/digit pp/letter) pp/letter])
+            "1a2b3")
   )
--- test/ssgr/markdown_test.clj
+++ test/ssgr/markdown_test.clj
@@ -4,6 +4,16 @@
 
 (deftest atx-heading
   (is (= (p/parse "# Heading 1")
-         [(p/heading 1 "Heading 1")]))
+         [(p/heading 1 [(p/text "Heading 1")])]))
   (is (= (p/parse "  ## Heading 2    \n")
-         [(p/heading 2 "Heading 2")])))
+         [(p/heading 2 [(p/text "Heading 2")])])))
+
+(deftest regular-text 
+  (is (= (p/parse "Texto normal")
+         [(p/paragraph [(p/text "Texto normal")])])))
+
+(deftest paragraph
+  (is (= (p/parse "P1. L1\nP1. L2\n\nP2. L1")
+         [(p/paragraph [(p/text "P1. L1")
+                        (p/text "P1. L2")])
+          (p/paragraph [(p/text "P2. L1")])])))
