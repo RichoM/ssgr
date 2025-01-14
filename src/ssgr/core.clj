974dc6abcc8e5e36aad365561e641d3dbb23cbb7
RichoM <richi.moran@gmail.com>
2025-01-14 13:21:11 -0300

    Changed core to actually parse and render the documents to html.
    Just for testing the entire workflow, even though the markdown parser only supports a small subset of all the syntax I wanted to see it in action with native-image and stuff.

--- src/ssgr/core.clj
+++ src/ssgr/core.clj
@@ -1,47 +1,10 @@
 (ns ssgr.core
   (:require [babashka.fs :as fs]
-            [markdown.core :as md :refer [md-to-html-string]]
             [clojure.string :as str]
-            [sci.core :as sci]
-            [clojure.java.io :as io]
-            [edamame.core :as e]
-            [clojure.tools.reader.reader-types :as r])
-  (:import [clojure.tools.reader.reader_types SourceLoggingPushbackReader])
+            [ssgr.parser :as p]
+            [ssgr.renderer :as r]
+            [hiccup.core :as h])
   (:gen-class))
-
-(comment
-
-  (def ;^SourceLoggingPushbackReader
-    r (-> (io/file "project.clj")
-          ;(io/reader)
-          (slurp)
-          (e/source-reader)))
-
-  (r/unread r \()
-  (r/peek-char r)
-  (r/read-char r)
-  (.read_char r)
-  [(e/get-line-number r)
-   (e/get-column-number r)]
-
-  (do (try
-        (when-let [ch (r/read-char r)]
-          (if (= ch \()
-            (do (r/unread r ch)
-                (let [obj (e/parse-next r #_(e/normalize-opts {:all false}))]
-                  (println "SUCCESS!" obj)
-                  (println (meta obj))))
-            (println ch)))        
-        (catch Exception ex
-          (println "ERROR!" (ex-message ex))))
-      [(e/get-line-number r)
-       (e/get-column-number r)])
-
-  (.close r)
-
-  (e/parse-string "richo")
-
-  )
 
 (defn write-file! [path contents]
   (let [components (fs/components path)]
@@ -51,7 +14,11 @@
 (defn process-file! [path out]
   (try
     (let [text (slurp (fs/file path))
-          html (md-to-html-string text)]
+          doc (p/parse text)
+          hiccup (r/render doc)
+          html (->> hiccup
+                    (map #(h/html %))
+                    (str/join "\n"))]
       (write-file! out html))
     (catch Exception ex
       (println (str "ERROR processing " path) ex))))
@@ -72,6 +39,11 @@
                          (str ".html"))]
         (println out-path)
         (process-file! path out-path)))))
+
+(comment
+  
+  (-main "doc" "out")
+  )
 
 (comment  
   (def src "doc")
