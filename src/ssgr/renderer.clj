bff35c0bb3268d0e133a1c1a5280ff8d75639371
RichoM <richi.moran@gmail.com>
2025-01-14 13:19:56 -0300

    Implemented a few more render cases

--- src/ssgr/renderer.clj
+++ src/ssgr/renderer.clj
@@ -1,5 +1,7 @@
 (ns ssgr.renderer
-  (:require [ssgr.doc :as doc]))
+  (:require [ssgr.doc :as doc]
+            [hiccup.core :as h]
+            [clojure.string :as str]))
 
 (defmulti render* :type)
 
@@ -12,13 +14,35 @@
                (map render* elements))))
 
 (defmethod render* ::doc/code [{:keys [form]}]
-  form)
+  (str form))
+
+(defmethod render* ::doc/paragraph [{:keys [lines]}]
+  (vec (concat [:p]
+               (mapcat render* lines))))
+
+(defmethod render* ::doc/line [{:keys [elements]}]
+  (mapv render* elements))
+
+(defmethod render* ::doc/document [{:keys [blocks]}] 
+  (mapv render* blocks))
 
 (defn render [document]
   (render* document))
 
 (comment
-  
+  (require '[hiccup.core :as h])
+
+  (concat [:p]
+          [1 2 23])
+  (h/html [:span "Richo capo"])
+  (h/html [:h1 [:span "Richo"]])
+
+  (->> (render (doc/document
+                (doc/heading 1 (doc/text "Main title"))
+                (doc/paragraph (doc/text-line "Richo capo"))))
+       (map #(h/html %))
+       (str/join "\n"))
+
   (render (doc/heading 1 (doc/text "Richo")
                        (doc/code '(+ 3 4))
                        (doc/text "Capo")))
