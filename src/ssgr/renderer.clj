28498566b2265fc41876ab2e49ca5b3406b13e6a
RichoM <richi.moran@gmail.com>
2025-06-18 22:58:01 -0300

    Added strong-emphasis to the renderer

--- src/ssgr/renderer.clj
+++ src/ssgr/renderer.clj
@@ -60,6 +60,11 @@
     (when (seq rendered-elements)
       (vec (concat [:em] rendered-elements)))))
 
+(defmethod render* ::doc/strong-emphasis [{:keys [text]}]
+  (let [rendered-elements (keep render text)]
+    (when (seq rendered-elements)
+      (vec (concat [:strong] rendered-elements)))))
+
 (defmethod render* ::doc/document [{:keys [blocks]}] 
   (let [rendered-blocks (keep render blocks)]
     (when (seq rendered-blocks)
