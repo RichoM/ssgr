d0cc7782208722a5e1115431d6b7e292dbfa13be
RichoM <richi.moran@gmail.com>
2025-08-16 19:10:44 -0300

    Implemented basic lexer

--- src/ssgr/lexer.clj
+++ src/ssgr/lexer.clj
@@ -1,81 +1,91 @@
 (ns ssgr.lexer
-  (:require [ssgr.input-stream :as in])
-  (:import java.io.StringReader
-           ssgr.InputStream))
+  (:require [ssgr.input-stream :as in]))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 
 (defn make-token
-  ([type start]
-   {:type type :start start :count 1})
-  ([type start count]
-   {:type type :start start :count count}))
+  ([src type char start]
+   {:src src :type type :char char :start start :count 1})
+  ([src type char start count]
+   {:src src :type type :char char :start start :count count}))
 
-;; (defprotocol Stream
-;;   (peek [stream])
-;;   (next! [stream])
-;;   (position [stream]))
+(defn letter? [chr]
+  (and chr (Character/isLetter ^char chr)))
 
-;; (deftype StringStream [src ^:unsynchronized-mutable ^long pos]
-;;   Stream
-;;   (peek [stream]
-;;         (let [^String src (.src stream)
-;;               pos (.pos stream)]
-;;           (when (< pos (.length src))
-;;             (char (.charAt src pos)))))
-;;   (next! [stream]
-;;     (when-let [val (peek stream)]
-;;       (set! pos (inc (.pos stream)))
-;;       val))
-;;   (position [stream]
-;;     (.pos stream)))
+(defn digit? [chr]
+  (and chr (Character/isDigit ^char chr)))
 
-(defn test-input-stream [src]
+(def space? #{\space \tab})
+
+(def newline-char? #{\return \newline})
+
+(defn skip-letters! [stream]
+  (loop []
+    (when (letter? (in/peek stream))
+      (in/next! stream)
+      (recur))))
+
+(defn tokenize! [src]
   (let [stream (in/make-stream src)]
-    (loop [result (transient [])]
+    (loop [tokens (transient [])
+           pos (in/position stream)]
       (if-let [next-char (in/next! stream)]
-        (recur (conj! result next-char))
-        (persistent! result)))))
+        (let [token (cond
+                      (letter? next-char)
+                      (do (skip-letters! stream)
+                          (make-token src ::word next-char 
+                                      pos 
+                                      (- ^long (in/position stream) 
+                                         ^long pos)))
 
-(defn test-string-reader [src]
-  (let [reader (StringReader. src)]
-    (loop [result (transient [])
-           idx 0]
-      (let [next-char (.read reader)]
-        (if-not (= -1 next-char)
-          (recur (conj! result (char next-char))
-                 (inc idx))
-          (persistent! result))))))
+                      (space? next-char)
+                      (make-token src ::space next-char pos)
 
-(defn test-input-stream2 [src]
-  (let [reader (InputStream. src)]
-    (loop [result (transient [])]
-      (let [next-char (.read reader)]
-        (if-not (= -1 next-char)
-          (recur (conj! result (char next-char)))
-          (persistent! result))))))
+                      (digit? next-char)
+                      (make-token src ::digit next-char pos)
+                      
+                      (newline-char? next-char)
+                      (if (= \return next-char)
+                        (if (= \newline next-char)
+                          (do (in/next! stream)
+                              (make-token src ::newline next-char pos 2))
+                          (make-token src ::newline next-char pos))
+                        (make-token src ::newline next-char pos))
+                      
+                      :else
+                      (make-token src ::symbol next-char pos))]
+          (recur (conj! tokens token)
+                 (in/position stream)))
+        (persistent! tokens)))))
 
 (comment
   (require 'user)
 
+  
   (nth "abc" 0)
   (get "abc" 10 nil)
-  
+
   (def src (slurp "test-files/intro.cljmd"))
+  (def src (slurp "test-files/03/02_UsoDeConsola.md"))
+  (count src)
+  (count (tokenize! src))
+  
+  (user/time+
+   (tokenize! src))
 
   (user/time+
    (dotimes [_ 100]
      (test-input-stream src)))
-  
+
   (user/time+
    (dotimes [_ 100]
      (test-string-reader src)))
-  
+
   (user/time+
    (dotimes [_ 100]
      (test-input-stream2 src)))
-  
+
   (user/time+
    (dotimes [_ 100]
      (test-string-stream src)))
